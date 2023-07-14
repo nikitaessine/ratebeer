28dccb214b9f21ab3355bec141e98af5efc7c8d9
Nikita Essine <nikita.essine@helsinki.fi>
2023-07-14 15:02:18 +0300

    2.4

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,5 +1,16 @@
 class Beer < ApplicationRecord
   belongs_to :brewery
   has_many :ratings
+
+  def average_rating
+    if ratings.empty?
+      return nil
+    else
+      total_scores = ratings.sum(:score)
+      total_ratings = ratings.count
+      average = total_scores.to_f / total_ratings
+      return average
+    end
+  end
 end
   
