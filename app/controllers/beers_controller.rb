ed07e1e76fc3cd12672375d8cb85bc1cd12af27e
Nikita Essine <nikita.essine@helsinki.fi>
2023-07-18 12:42:08 +0300

    2.10

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -13,6 +13,8 @@ class BeersController < ApplicationController
   # GET /beers/new
   def new
     @beer = Beer.new
+    @breweries = Brewery.all
+    @styles = ["Weizen", "Lager", "Pale ale", "IPA", "Porter", "Lowalcohol"]
   end
   # GET /beers/1/edit
   def edit
