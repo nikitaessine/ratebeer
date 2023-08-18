c9ea4f5588d1a14ac092040f58f25f5969bea94e
Nikita Essine <nikita.essine@helsinki.fi>
2023-08-18 12:06:07 +0300

    3.4

--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,7 @@ Rails.application.routes.draw do
   resource :session, only: [:new, :create, :destroy]
 
   get 'signup', to: 'users#new'
+  get 'signin', to: 'sessions#new'
+  delete 'signout', to: 'sessions#destroy'
 
 end
