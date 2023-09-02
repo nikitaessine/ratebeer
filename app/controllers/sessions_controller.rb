592b6e1fcc4023e941e6c2da17f35bfaa199830b
Nikita Essine <nikita.essine@helsinki.fi>
2023-09-02 13:11:26 +0300

    3.7

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,13 @@ class SessionsController < ApplicationController
     end
   
     def create
-      # haetaan usernamea vastaava käyttäjä tietokannasta
-      user = User.find_by username: params[:username]
-      # talletetaan sessioon kirjautuneen käyttäjän id (jos käyttäjä on olemassa)
-      session[:user_id] = user.id if not user.nil?
-      # uudelleen ohjataan käyttäjä omalle sivulleen
-      redirect_to user
+        user = User.find_by username: params[:username]
+        if user.nil?
+          redirect_to signin_path, notice: "User #{params[:username]} does not exist!"
+        else
+          session[:user_id] = user.id
+          redirect_to user, notice: "Welcome back!"
+        end
     end
   
     def destroy
