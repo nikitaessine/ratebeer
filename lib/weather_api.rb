e93f0c5244744fd3e048ff6e923a92fc2a16f16d
Nikita Essine <nikita.essine@helsinki.fi>
2023-12-07 16:20:57 +0200

    5.15-16

---
+++ lib/weather_api.rb
@@ -0,0 +1,18 @@
+class WeatherApi
+    def self.weather_in(city)
+        city = city.downcase
+        Rails.cache.fetch("#{city}_weather", expires_in: 1.week) { get_weather_in(city) }
+    end
+    
+    def self.get_weather_in(city)
+        url = "http://api.weatherstack.com/current?access_key=#{key}&query="
+        response = HTTParty.get "#{url}#{ERB::Util.url_encode(city)}"
+        response.parsed_response["current"]
+    end
+  
+    def self.key
+      return nil if Rails.env.test? # no API needed when testing, return nil
+      raise 'WEATHER_APIKEY env variable not defined' if ENV['WEATHER_APIKEY'].nil?
+      ENV.fetch('WEATHER_APIKEY')
+    end
+end
--- spec/features/places_spec.rb
+++ spec/features/places_spec.rb
@@ -1,6 +1,34 @@
 require 'rails_helper'
 
 describe "Places" do
+
+  let(:mock_weather) do
+    {
+      "observation_time" => "10:01 PM",
+      "temperature" => 10,
+      "weather_code" => 296,
+      "weather_icons" => ["https://cdn.worldweatheronline.com/images/wsymbols01_png_64/wsymbol_0033_cloudy_with_light_rain_night.png"],
+      "weather_descriptions" => ["Light Rain, Mist"],
+      "wind_speed" => 13,
+      "wind_degree" => 190,
+      "wind_dir" => "S",
+      "pressure" => 1001,
+      "precip" => 0,
+      "humidity" => 89,
+      "cloudcover" => 100,
+      "feelslike" => 8,
+      "uv_index" => 1,
+      "visibility" => 6,
+      "is_day" => "no"
+    }
+  end
+  
+  before do
+    ["helsinki", "madrid", "new york"].each do |city|
+      allow(WeatherstackApi).to receive(:weather_in).with(city).and_return(mock_weather)
+    end
+  end
+
   context "when API returns results" do
     it "displays all returned places on the page" do
       allow(BeermappingApi).to receive(:places_in).with("kumpula").and_return(
